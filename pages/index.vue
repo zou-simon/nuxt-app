3759526bdc58547712572dc53ad9a5e3e13a2250
Simon <zou-simon@hotmail.fr>
2022-05-31 00:50:12 +0200

    Update home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,24 @@
 <template>
-    <div class="grid lg:grid-rows-3 lg:grid-flow-row gap-4 xs:gap-6 p-4 xs:p-6 h-screen">
-        <div class="sm:col-span-2 md:col-span-1 row-span-5 lg:row-span-4 rounded-lg bg-center bg-cover grid content-evenly place-items-center p-4"
-            style="background-image: url('/assets/devanture.jpg')">
-            <div class="text-center">
+    <main class="md:grid md:grid-rows-3 md:grid-cols-4 md:grid-flow-col md:gap-4 md:min-h-screen p-4 pb-2 md:pb-4">
+        <div class="flex flex-col md:row-span-3 md:col-span-2 bg-center bg-cover md:grid md:content-evenly place-items-center p-4 mb-4 md:mb-0" id="bg-devanture">
+            <div class="text-center mb-4">
                 <h1 class="text-5xl md:text-7xl font-black uppercase">Tokyo Yaki</h1>
                 <p class="text-md md:text-xl font-light tracking-wider">Restaurant Japonais</p>
             </div>
 
             <a href="https://maps.google.com/?q=Tokyo+Yaki+75020"
-                class="uppercase bg-white text-black font-bold rounded-full text-center py-2 w-1/2 hover:scale-105 transition delay-100 duration-300 ease-in-out">
-                Y aller
+                class="uppercase bg-white text-black font-bold text-center border-4 border-black py-2 px-4 md:w-1/2 hover:bg-neutral-300">
+                Y&nbsp;aller
             </a>
         </div>
-        <div class="sm:col-span-2 lg:row-span-2 rounded-lg bg-primary p-4 sm:p-6 md:p-8">
-            <h2 class="text-2xl uppercase font-bold tracking-wider">À propos</h2>
+        <div class="md:row-span-2 md:col-span-2 bg-primary p-4 md:p-8">
+            <h2 class="text-xl md:text-2xl uppercase font-bold tracking-wider mb-1">À propos</h2>
             <div class="sm:text-lg md:text-xl lg:text-2xl space-y-6">
                 <div>
                     <p>
                         253 Rue des Pyrénées, 75020 Paris
                     </p>
-                    <a href="tel:+33143150371" class="hover:text-neutral-300">
+                    <a href="tel:+33143150371" class="hover:text-neutral-300 hover:underline">
                         +33 1 43 15 03 71
                     </a>
                 </div>
@@ -32,7 +31,7 @@
                 </div>
                 <div>
                     <p>
-                        Livraison à partir de 15 €
+                        Livraison dans le 20e à partir de 15 €
                     </p>
                     <p class="text-red-500">
                         -10% à emporter sauf les desserts et boissons
@@ -40,17 +39,21 @@
                 </div>
             </div>
         </div>
-        <div class="sm:col-auto sm:row-span-2 md:row-span-4 lg:row-span-2 rounded-lg bg-primary grid content-center justify-items-center p-4">
-            <a href="#"
-                class="uppercase bg-white text-black font-bold rounded-full text-center p-2 w-3/4 hover:scale-105 transition delay-100 duration-300 ease-in-out">
-                Commander
-            </a>
+        <div class="sticky md:static bottom-2 flex md:col-span-2 space-x-2 md:space-x-4 my-2 md:my-0">
+            <NuxtLink to="#"
+                activeClass="w-full group grid md:content-center md:justify-center md:bg-center md:bg-cover md:bg-primary md:bg-no-repeat hover:bg-contain" id="bg-chirashi">
+                <span
+                    class="uppercase bg-white text-black font-bold w-full md:w-auto text-center md:border-4 md:border-black p-2 md:py-3 md:px-6 group-hover:bg-neutral-300">
+                    Commander
+                </span>
+            </NuxtLink>
+            <NuxtLink to="#"
+                activeClass="w-full group grid md:content-center md:justify-center md:bg-center md:bg-cover md:bg-primary md:bg-no-repeat hover:bg-contain" id="bg-brochettes">
+                <span
+                    class="uppercase bg-white text-black font-bold w-full md:w-auto text-center md:border-4 md:border-black p-2 md:py-3 md:px-6 group-hover:bg-neutral-300">
+                    Menu
+                </span>
+            </NuxtLink>
         </div>
-        <div class="sm:col-auto sm:row-span-2 md:row-span-4 lg:row-span-2 rounded-lg bg-primary grid content-center justify-items-center p-4">
-            <a href="#"
-                class="uppercase bg-white text-black font-bold rounded-full text-center p-2 w-3/4 hover:scale-105 transition delay-100 duration-300 ease-in-out">
-                Menu
-            </a>
-        </div>
-    </div>
+    </main>
 </template>
